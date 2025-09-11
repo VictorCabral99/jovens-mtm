a237ea859c06203962b663ec0ffa37e61698f34b
Victor Cabral <vhcabral99@gmail.com>
2025-09-10 23:52:51 -0300

    add meta viewport

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ app.get("/vapid-public-key", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
