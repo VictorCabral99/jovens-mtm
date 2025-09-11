d5a5c72a252a59941e251f086c9c28c82a00fa1e
Victor Cabral <vhcabral99@gmail.com>
2025-09-10 23:28:54 -0300

    remove import, add require

--- backend.js
+++ backend.js
@@ -1,7 +1,7 @@
-import express from "express";
-import bodyParser from "body-parser";
-import webpush from "web-push";
-import dotenv from "dotenv";
+const express = require("express");
+const bodyParser = require("body-parser");
+const webpush = require("web-push");
+const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
